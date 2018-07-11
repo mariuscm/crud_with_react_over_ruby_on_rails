68a6bef61f6ce42dfab0b04985e624ede0cb5fdd
Marius Maries <mariuscm@gmail.com>
2018-07-10 19:12:04 -0500

    set up the controllers

---
+++ app/config/routes.rb
@@ -0,0 +1,4 @@
+#app/config/routes.rb
+Rails.application.routes.draw do
+  root to: 'site#index'
+end
---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,21 @@
+#items_controller.rb
+
+class Api::V1::ItemsController < Api::V1::BaseController
+  def index
+    respond_with Item.all
+  end
+
+  def create
+    respond_with :api, :v1, Item.create(item_params)
+  end
+
+  def update
+    item = Item.find(params["id"])
+    item.update_attributes(item_params)
+    respond_with item, json: item
+  end
+
+  def destroy
+    respond_with Item.destroy(params[:id])
+  end
+end
